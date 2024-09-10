a2b6192345575376a369ae22befa22f9b9100200
Micah Knox <miknox0627@gmail.com>
2024-09-10 15:51:06 -0500

    added Robert as code contributer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Code by Micah Knox   Art by Space Monkey Mafia   started 09-09-24
+#Code by Micah Knox and Robert Palmer   Art by Space Monkey Mafia   started 09-09-24
 #
 import math
 import pygame
