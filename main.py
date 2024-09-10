89a95ef8e3a244a7be96f050fb91f80c3a214e04
Micah Knox <miknox0627@gmail.com>
2024-09-10 16:14:36 -0500

    removed robert so he can do it himself

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Code by Micah Knox and Robert Palmer   Art by Space Monkey Mafia   started 09-09-24
+#Code by Micah Knox   Art by Space Monkey Mafia   started 09-09-24
 #
 #https://stackoverflow.com/questions/73378518/how-to-import-and-call-main-functions-from-different-files
 #
