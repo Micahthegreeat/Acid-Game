32c668e67855d4fa5b5518fae2ff4192f64d535e
Micah Knox <miknox0627@gmail.com>
2024-09-10 15:53:35 -0500

    added resource

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #Code by Micah Knox and Robert Palmer   Art by Space Monkey Mafia   started 09-09-24
 #
+#https://stackoverflow.com/questions/73378518/how-to-import-and-call-main-functions-from-different-files
+#
+#
 import math
 import pygame
 import time
